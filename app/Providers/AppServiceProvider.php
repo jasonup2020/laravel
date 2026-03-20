b044c19c95bfaea3d0134c6dbee85ac077d8e35e
jasonup2020 <85090516+jasonup2020@users.noreply.github.com>
2026-03-20 15:50:29 +0800

    //加载自动定时任务

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,18 @@
 
 namespace App\Providers;
 
+use App\Console\Kernel;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+		//加载自动定时任务
+        $this->app->singleton(ConsoleKernelContract::class, Kernel::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
